c8d566f4715e908471d11755a3047369cf4d424e
smagikern <smagikern@hotmail.com>
2014-11-17 03:41:19 +0300

    Update and rename 0014_auto__chg_field_attachmentrevision_previous_revision__chg_field_revisi.py to 0014_bauto__chg_field_attachmentrevision_previous_revision__chg_field_revisi.py

--- wiki/south_migrations/0014_bauto__chg_field_attachmentrevision_previous_revision__chg_field_revisi.py
+++ wiki/south_migrations/0014_bauto__chg_field_attachmentrevision_previous_revision__chg_field_revisi.py
@@ -4,6 +4,7 @@ from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
+user_orm_label = '%s.%s' % (User._meta.app_label, User._meta.object_name)
 
 class Migration(SchemaMigration):
 
