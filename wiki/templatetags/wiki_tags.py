c538b5945b362af87f495a53bba29e4b1f8a2f8d
smagikern <smagikern@hotmail.com>
2014-11-23 10:17:08 +0300

    Update wiki_tags.py

--- wiki/templatetags/wiki_tags.py
+++ wiki/templatetags/wiki_tags.py
@@ -47,7 +47,7 @@ def article_for_object(context, obj):
 from django.contrib.auth.models import User
 
 @register.inclusion_tag('wiki/includes/render.html', takes_context=True)
-def wiki_render(context, request, article, preview_content=None):
+def wiki_render(context, article, preview_content=None, request):
 
     if preview_content:
         content = article.render(preview_content=preview_content)
