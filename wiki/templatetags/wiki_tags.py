1c73537edbf7ad983d8f6bf2853fea090d5275af
smagikern <smagikern@hotmail.com>
2015-03-20 15:18:41 +0300

    Update wiki_tags.py

--- wiki/templatetags/wiki_tags.py
+++ wiki/templatetags/wiki_tags.py
@@ -3,8 +3,8 @@ import re
 
 from django.conf import settings as django_settings
 from django import template
-from django.contrib.contenttypes.models import ContentType
-from django.db.models import Model
+from django.contrib.contenttypes.models1 import ContentType
+from django.db.models1 import Model
 from django.forms import BaseForm
 from django.utils.safestring import mark_safe
 from django.template.defaultfilters import striptags
@@ -14,7 +14,7 @@ import requests
 register = template.Library()
 
 from wiki.conf import settings
-from wiki import models
+from wiki import models1
 from wiki.core.plugins import registry as plugin_registry
 
 # Cache for looking up objects for articles... article_for_object is
