67fbf7fcea025ea44d7764fc9cf1f0be5b46d796
smagikern <smagikern@hotmail.com>
2015-03-24 18:07:18 +0300

    Update wiki_tags.py

--- wiki/templatetags/wiki_tags.py
+++ wiki/templatetags/wiki_tags.py
@@ -64,8 +64,9 @@ def article_for_object(context, obj):
     return _cache[obj]
 
 @register.simple_tag(takes_context=True)
-def current_read(request):
-    user= articleread.objects.create(read='True',user=self.request.user.id,article=article.id, paid='False',readed=datetime.datetime.now())    
+def current_read(context):
+    request = context['request']
+    user= articleread.objects.create(read='True',user=request.user.id,article=article.id, paid='False',readed=datetime.datetime.now())    
     user.save()
     return datetime.datetime.now()
     
