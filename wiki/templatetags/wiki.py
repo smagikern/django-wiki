257d939a4f24ed2388ee6143af3be71f119c1d46
smagikern <smagikern@hotmail.com>
2015-03-24 17:58:25 +0300

    Update wiki.py

--- wiki/templatetags/wiki.py
+++ wiki/templatetags/wiki.py
@@ -7,7 +7,7 @@ from myproject.models import WikiArticleread as articleread
 register = template.Library()
 
 @register.assignment_tag(takes_context=True)
-def current_read(self.request):
+def current_read(self):
     user= articleread.objects.create(read='True',user=self.request.user.id,article=article.id, paid='False',readed=datetime.datetime.now())    
     user.save()
     return datetime.datetime.now()
